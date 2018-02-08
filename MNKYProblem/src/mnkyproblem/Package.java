5442f639cac33e22953a8ddba17980b03a076347
Larry <larryabim@gmail.com>
2018-02-08 17:06:04 -0600

    Added stuff to Packages

--- MNKYProblem/src/mnkyproblem/Package.java
+++ MNKYProblem/src/mnkyproblem/Package.java
@@ -9,6 +9,19 @@ package mnkyproblem;
  *
  * @author victor
  */
-public class Package {
-    
+public class Package extends Location{
+    float post = 0;
+    float dest = 0;
+
+    public Package(float p, float d){
+        post = p;
+        setX(post);
+        dest = d;
+    }
+    public float getPost(){
+        return post;
+    }
+    public float getDest(){
+        return dest;
+    }
 }
